fd9d3a134010a77600c05a86cb6d100c3d6aa790
Kamil Ogórek <kamil.ogorek@gmail.com>
2022-07-27 13:22:06 +0200

    Working port

---
+++ src/fixtures/bundle.js
@@ -0,0 +1,2 @@
+function n(){throw new Error("hi")}function o(){n()}o();
+//# sourceMappingURL=bundle.js.map
---
+++ src/main.rs
@@ -0,0 +1,176 @@
+use std::{collections::HashMap, fs::read_to_string, io::BufRead};
+
+use sourcemap::{SourceMap, SourceView};
+
+#[derive(Debug, Default)]
+struct Frame {
+    pub abs_path: String,
+    pub lineno: u32,
+    pub colno: u32,
+    pub context_line: Option<String>,
+    pub pre_context: Option<String>,
+    pub post_context: Option<String>,
+    pub function: String,
+}
+
+#[derive(Debug)]
+struct Stacktrace {
+    pub frames: Vec<Frame>,
+}
+
+struct Processor<'a> {
+    pub sources: HashMap<String, SourceView<'a>>,
+    pub sourcemaps: HashMap<String, SourceMap>,
+    pub links: HashMap<String, String>,
+}
+
+impl<'a> Processor<'a> {
+    fn new() -> Self {
+        Processor {
+            sources: HashMap::new(),
+            sourcemaps: HashMap::new(),
+            links: HashMap::new(),
+        }
+    }
+    fn preprocess_step(&mut self, frames: &[Frame]) {
+        self.populate_source_cache(frames);
+    }
+
+    fn populate_source_cache(&mut self, frames: &[Frame]) {
+        for frame in frames {
+            self.cache_source(frame.abs_path.to_string());
+        }
+    }
+
+    fn cache_source(&mut self, abs_path: String) {
+        let source_content = read_to_string(format!("src/fixtures/{}", abs_path)).unwrap();
+        let source_view = SourceView::from_string(source_content);
+
+        let sourcemap_ref = source_view.sourcemap_reference().unwrap().unwrap();
+        let sourcemap_ref_url = sourcemap_ref.get_url();
+
+        let sourcemap_content =
+            read_to_string(format!("src/fixtures/{}", sourcemap_ref_url)).unwrap();
+        let sourcemap = SourceMap::from_slice(sourcemap_content.as_bytes()).unwrap();
+
+        let sm_sources = sourcemap.sources();
+        for (idx, source) in sm_sources.enumerate() {
+            let inline_source_content = sourcemap.get_source_view(idx as u32).unwrap().source();
+            let inline_source_view = SourceView::from_string(inline_source_content.to_string());
+            self.sources.insert(source.to_string(), inline_source_view);
+        }
+
+        self.sources.insert(abs_path.to_string(), source_view);
+        self.sourcemaps
+            .insert(sourcemap_ref_url.to_string(), sourcemap);
+        self.links
+            .insert(abs_path.to_string(), sourcemap_ref_url.to_string());
+    }
+
+    fn process_frame(&mut self, frame: &mut Frame) {
+        // frame = processable_frame.frame
+
+        // cache = self.cache
+        // sourcemaps = self.sourcemaps
+        // all_errors = []
+        // sourcemap_applied = False
+
+        // let source = self.sources.get(&frame.abs_path).unwrap();
+
+        // new_frame = dict(frame)
+        // raw_frame = dict(frame)
+
+        let sourcemap_url = self.links.get(&frame.abs_path).unwrap();
+        let sourcemap_view = self.sourcemaps.get(sourcemap_url).unwrap();
+
+        // let minified_function_name = &frame.function;
+        let minified_source = self.sources.get(&frame.abs_path).unwrap();
+
+        let token = sourcemap_view
+            .lookup_token(frame.lineno - 1, frame.colno - 1)
+            .unwrap();
+
+        // This is the place where we actually modify the frame
+        frame.lineno = token.get_src_line() + 1;
+        frame.colno = token.get_src_col() + 1;
+        frame.function = minified_source
+            .get_original_function_name(token, &frame.function)
+            .unwrap_or("<unknown>")
+            .to_string();
+
+        let abs_path = token.get_source().unwrap().to_string();
+        frame.abs_path = abs_path.clone();
+
+        let unminified_source = self.sources.get(&abs_path).unwrap();
+
+        self.expand_frame(frame, unminified_source);
+    }
+
+    fn expand_frame(&self, frame: &mut Frame, source: &SourceView) {
+        let (pre, line, post) = get_source_context(source, frame.lineno as u32);
+
+        frame.pre_context = pre;
+        frame.context_line = line;
+        frame.post_context = post;
+    }
+}
+
+fn get_source_context(
+    source: &SourceView,
+    lineno: u32,
+) -> (Option<String>, Option<String>, Option<String>) {
+    (
+        source.get_line(lineno - 1).map(String::from),
+        source.get_line(lineno).map(String::from),
+        source.get_line(lineno + 1).map(String::from),
+    )
+}
+
+fn main() {
+    let mut processor = Processor::new();
+
+    let frames = vec![
+        Frame {
+            abs_path: String::from("bundle.js"),
+            function: "n".to_string(),
+            lineno: 1,
+            colno: 20,
+            ..Default::default()
+        },
+        Frame {
+            abs_path: String::from("bundle.js"),
+            function: "o".to_string(),
+            lineno: 1,
+            colno: 49,
+            ..Default::default()
+        },
+        Frame {
+            abs_path: String::from("bundle.js"),
+            function: "Object.<anonymous>".to_string(),
+            lineno: 1,
+            colno: 53,
+            ..Default::default()
+        },
+    ];
+
+    let mut stacktrace = Stacktrace { frames };
+
+    dbg!(&stacktrace);
+
+    processor.preprocess_step(&stacktrace.frames);
+
+    for frame in &mut stacktrace.frames {
+        processor.process_frame(frame);
+    }
+
+    dbg!(&stacktrace);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
